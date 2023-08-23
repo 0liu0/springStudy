37dc96a37b186ffdfecf0ed773d8393b5917b9aa
liuche <706716852@qq.com>
2023-08-23 20:49:58 +0800

    liuche 分支第一次提交

---
+++ liuche-test/src/com/liuche/Main.java
@@ -0,0 +1,14 @@
+package com.liuche;
+
+/**
+ * @Author 刘彻
+ * @Date 2023/8/23 20:48
+ * @PackageName: com.liuche
+ * @ClassName: Main
+ * @Description: TODO
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("你好啊！");
+    }
+}
